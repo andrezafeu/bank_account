fc57cabc7d2f66ae7cfb6e2be5ff4f5534d9b8c3
Andreza Feu <andrezafeu@gmail.com>
2018-02-23 00:50:37 -0500

    Add method add_transaction

--- bank_account.rb
+++ bank_account.rb
@@ -1,9 +1,19 @@
 class BankAccount
+  attr_reader :name
+  
   def initialize(name)
     @name = name
     @transactions = []
+    add_transaction("Beginning balance", 0)
   end
-end
 
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
+  end
+
+end
+  
 bank_account = BankAccount.new("Andreza")
+puts bank_account.inspect
+bank_account.add_transaction("groceries", 20)
 puts bank_account.inspect
