fe13a2288ee113696ec504f20425c35c60c591d0
Andreza Feu <andrezafeu@gmail.com>
2018-02-23 00:57:35 -0500

    Separate transactions between debit and credit

--- bank_account.rb
+++ bank_account.rb
@@ -6,6 +6,14 @@ class BankAccount
     @transactions = []
     add_transaction("Beginning balance", 0)
   end
+  
+  def credit(description, amount)
+    add_transaction(description, amount)
+  end
+  
+  def debit(description, amount)
+    add_transaction(description, -amount)
+  end
 
   def add_transaction(description, amount)
     @transactions.push(description: description, amount: amount)
@@ -14,6 +22,7 @@ class BankAccount
 end
   
 bank_account = BankAccount.new("Andreza")
+bank_account.credit("paycheck", 100)
 puts bank_account.inspect
-bank_account.add_transaction("groceries", 20)
+bank_account.debit("groceries", 20)
 puts bank_account.inspect
